6a46acf8f5de3e9145b432cd919bbbeec522ee60
BigYuki <bigyuki@yeah.net>
2015-11-12 13:19:56 +0800

    边栏分角色显示

--- front-end/web/js/main.js
+++ front-end/web/js/main.js
@@ -2,7 +2,7 @@
 
 require.config({
   //  baseUrl: "/js/",
-  urlArgs: "version=0.0.2",
+  urlArgs: "version=0.0.3",
   paths: {
     text                  : 'libs/text/text',
     jquery                : 'libs/jquery/dist/jquery.min',  
--- front-end/web/js/views/base/SidebarView.js
+++ front-end/web/js/views/base/SidebarView.js
@@ -1,33 +1,50 @@
-define([ 'jquery', 'underscore', 'backbone',
-         'text!templates/base/sidebarTemplate.html' ], function($, _, Backbone,
-                 sidebarTemplate) {
+define(['jquery', 'underscore', 'backbone',
+    'models/user/UserModel',
+    'text!templates/base/sidebarTemplate.html'], function ($, _, Backbone,
+        UserModel,
+        sidebarTemplate) {
 
-    var SidebarView = Backbone.View.extend({
+        var SidebarView = Backbone.View.extend({
 
-        initialize : function() {
-            this.render();
-        },
-        events : {
-            'click li a[data-toggle="page"]'         : "navigate",
-        },
+            initialize: function () {
+                
+                
+                this.user = new UserModel();
+                this.user.fetch({async:false});
+                this.is_regulator = this.user.get('is_regulator');
+                console.log( this.is_regulator );
+                this.render();
+            },
+            events: {
+                'click li a[data-toggle="page"]': "navigate",
+            },
 
-        navigate : function(e){
-            e.preventDefault();
-            Backbone.history.navigate(e.currentTarget.getAttribute('href'), {trigger:true});
-        },
+            navigate: function (e) {
+                e.preventDefault();
+                Backbone.history.navigate(e.currentTarget.getAttribute('href'), { trigger: true });
+            },
 
-        render : function() {
-            this.$el.html(sidebarTemplate);
-            this.$('#side-menu').metisMenu();
-            var url = Backbone.history.fragment;
-            this.$('.nav li').removeClass('active');
-            this.$('.nav li a[href="'+url+'"][data-toggle="page"]').addClass('active');
+            render: function () {
+                
+                var templateObject = _.template(sidebarTemplate); 
+                this.$el.html(templateObject(
+                {
+                _:_,
+                is_regulator: this.is_regulator
+                }
+                ));
+                
+                
+                this.$('#side-menu').metisMenu();
+                var url = Backbone.history.fragment;
+                this.$('.nav li').removeClass('active');
+                this.$('.nav li a[href="' + url + '"][data-toggle="page"]').addClass('active');
 
-            this.$('.nav li ul li a[class="active"]').parent().parent().addClass('in');
-        }
+                this.$('.nav li ul li a[class="active"]').parent().parent().addClass('in');
+            }
+
+        });
+
+        return SidebarView;
 
     });
-
-    return SidebarView;
-
-});
